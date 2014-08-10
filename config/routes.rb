e8f82c54a3e0b5e616bc0af5ddbfcce3e3c39733
Henk Tillman <henk.tillman@gmail.com>
2014-08-09 19:54:35 -0700

    Adds versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Api definitions
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    # List resources here
+    scope module: :v1, constraints: ApiContraints.new(version: 1, default: true) do
+      # List resources here
+    end
   end  
 end
